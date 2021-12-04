f588b8e06147c8c4c65b954440b4c99ce0ca341f
Teerapat Kraisrisirikul <teerapat_saint@hotmail.com>
2021-12-04 17:59:35 +0700

    🔥 v1.3.2
    🔥 removes set-up and run script

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ import numpy
 
 APP_NAME = 'Progress Tracker Application'
 AUTHOR = '810Teams'
-VERSION = 'v1.3.0'
+VERSION = 'v1.3.2'
 OPERATIONS = [
     Operation('a', 'append', 'Append Data', [
         Argument('-add', 'Add mode'),
@@ -53,13 +53,13 @@ def main():
     show_app_title()
 
     if load_default_storage():
-        storage_main = Storage(load_default_storage()) 
+        storage_main = Storage(load_default_storage())
     else:
         notice('Please Input Storage Name')
         print()
         storage_main = Storage(input('(Input) ').strip())
         print()
-    
+
     if storage_main.try_load():
         notice('File \'DEFAULT_STORAGE.txt\' is found. Proceeding to storage loading.')
         notice('Storage \'{}\' is loaded.'.format(storage_main.name))
@@ -108,7 +108,7 @@ def operate(storage_main, action, args):
     except (NameError, SyntaxError):
         try:
             eval('operate_{}(storage_main, args)'.format([i.command for i in OPERATIONS if i.code == action.lower()][0]))
-        except (IndexError, NameError, SyntaxError): 
+        except (IndexError, NameError, SyntaxError):
             error('Invalid action. Please try again.')
 
 main()
