117e55d6009dfef88e7baac45b67d8597ff2432a
Teerapat Kraisrisirikul <teerapat_saint@hotmail.com>
2024-06-06 13:25:28 +0900

    ⚗️ v3.0.0 Updated custom append file

--- custom/append.py
+++ custom/append.py
@@ -15,7 +15,7 @@ def custom_append_head(data, custom_id):
 
 def custom_append_1(data):
     """ Custom Append Function #1 """
-    data = [[int(j) for j in i.split()] for i in data.strip().split(',')]
+    data = [[int(j) for j in i.split('-')] for i in data.strip().split(',')]
     return [(i[0] - 1) * 16 + i[1] - 1 for i in data]
 
 
@@ -23,21 +23,25 @@ def custom_append_2(data):
     """ Custom Append Function #2 """
     # Your custom code here!
     # Don't forget to return calculated value
+    pass
 
 
 def custom_append_3(data):
     """ Custom Append Function #3 """
     # Your custom code here!
     # Don't forget to return calculated value
+    pass
 
 
 def custom_append_4(data):
     """ Custom Append Function #4 """
     # Your custom code here!
     # Don't forget to return calculated value
+    pass
 
 
 def custom_append_5(data):
     """ Custom Append Function #5 """
     # Your custom code here!
     # Don't forget to return calculated value
+    pass
