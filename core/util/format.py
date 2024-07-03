9408b150ae7b5ae44ca69b9b48d1277dc4ad0bf4
Teerapat Kraisrisirikul <teerapat_saint@hotmail.com>
2024-07-03 13:09:22 +0900

    ♻️ v3.0.0 Minor refactor

--- core/util/format.py
+++ core/util/format.py
@@ -2,10 +2,10 @@
     `core/util/format.py`
 """
 
-def path(*path_list: tuple[str]) -> str:
+def path(*path_segment_list: tuple[str]) -> str:
     """ Function: Format path """
-    path_list = [p.strip() for p in path_list if p.strip() != str()]
-    joined_path = '/'.join([p.strip('/') for p in path_list])
+    path_segment_list: list[str] = [segment.strip() for segment in path_segment_list if segment.strip() != str()]
+    joined_path: str = '/'.join(path_segment_list)
 
     while '//' in joined_path:
         joined_path = joined_path.replace('//', '/')
