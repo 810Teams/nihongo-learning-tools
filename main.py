e770825be7fa140f372f2e94930d2008995e363d
Teerapat Kraisrisirikul <teerapat_saint@hotmail.com>
2024-07-31 11:51:02 +0900

    🥅 v3.0.0 Added cache clearing when application encounters runtime error

--- main.py
+++ main.py
@@ -2,16 +2,30 @@
     `main.py`
 """
 
+import os
+
+from core.util.logging import error
 from core.settings import INSTALLED_APPLICATIONS
+from settings import ENABLE_PYCLEAN
 
 
 def main() -> None:
     """ Main function """
     index: int = 0
-    while True:
-        INSTALLED_APPLICATIONS[index].start()
-        index += 1
-        index %= len(INSTALLED_APPLICATIONS)
+    try:
+        while True:
+            INSTALLED_APPLICATIONS[index].setup()
+            INSTALLED_APPLICATIONS[index].start()
+            index += 1
+            index %= len(INSTALLED_APPLICATIONS)
+    except RuntimeError:
+        print()
+        error('Unexpected error occured, forcing the application to close.')
+        error('Please contact application developer for further investigation.')
+        if ENABLE_PYCLEAN:
+            print()
+            os.system('pyclean .')
+            print()
 
 
 if __name__ == '__main__':
